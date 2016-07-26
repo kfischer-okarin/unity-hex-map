bb998b68db320c05e8f285fc315015bf935ce6d2
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2016-07-26 12:29:21 +0900

    Add Basic HexMapRenderer Component

---
+++ Assets/HexMap/Components/HexMapRenderer.cs
@@ -0,0 +1,124 @@
+﻿/* Copyright (c) 2016 Kevin Fischer
+ *
+ * This Source Code Form is subject to the terms of the MIT License.
+ * If a copy of the license was not distributed with this file,
+ * You can obtain one at https://opensource.org/licenses/MIT. */
+
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace HexMapEngine {
+
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(MeshRenderer), typeof(MeshFilter))]
+    public class HexMapRenderer : MonoBehaviour {
+
+        [SerializeField] HexMap _hexMap;
+
+        public HexMap HexMap {
+            get { return _hexMap; }
+            set {
+                _hexMap = value;
+                GenerateMesh();
+            }
+        }
+
+        public float gridSize = 1;
+
+        void Start() {
+            if (_hexMap != null)
+                GenerateMesh();
+        }
+
+        float _lastGridSize;
+
+        void OnValidate() {
+            if (gridSize != _lastGridSize && _hexMap != null) {
+                _lastGridSize = gridSize;
+                GenerateMesh();
+            }
+        }
+
+        #region Mesh Generation
+        // Pointy top, q right, r bottom-right
+        Vector2 GetHexCenter(HexCell cell) {
+            return new Vector2(gridSize * Mathf.Sqrt(3) * (cell.q + cell.r / 2f), gridSize * (-3f / 2f) * cell.r);
+        }
+
+        // 0 bottom right, 1 bottom, .. 5 top right
+        Vector2 GetHexCorner(Vector2 center, int corner) {
+            var angle_deg = 60 * corner + 30;
+            var angle_rad = Mathf.PI / 180 * angle_deg;
+            return new Vector2(center.x + gridSize * Mathf.Cos(angle_rad), center.y - gridSize * Mathf.Sin(angle_rad));
+        }
+
+        public void GenerateMesh() {
+            var newMesh = new Mesh();
+            newMesh.hideFlags = HideFlags.HideAndDontSave;
+
+            _gridPoints.Clear();
+            var vertices = new List<Vector3>();
+            var triangles = new List<int>();
+            var uvs = new List<Vector2>();
+
+            foreach (HexCell c in _hexMap.hexCells) {
+                Vector2 center = GetHexCenter(c);
+                vertices.Add(center);
+                uvs.Add(Vector2.zero);
+
+                int centerIndex = vertices.Count - 1;
+                var corners = new int[6];
+
+                for (int i = 0; i < 6; i++) {
+                    Vector2 corner = GetHexCorner(center, i);
+                    if (vertices.Contains(corner)) {
+                        corners[i] = vertices.IndexOf(corner);
+                    } else {
+                        vertices.Add(corner);
+                        uvs.Add(Vector2.zero);
+                        corners[i] = vertices.Count - 1;
+                    }
+
+                    _gridPoints.Add(corner);
+                }
+
+                for (int i = 0; i < 6; i++) {
+                    triangles.AddRange(new int[] { centerIndex, corners[i], corners[i == 5 ? 0 : i + 1] });
+                }
+            }
+
+            newMesh.vertices = vertices.ToArray();
+            newMesh.uv = uvs.ToArray();
+            newMesh.triangles = triangles.ToArray();
+            GetComponent<MeshFilter>().sharedMesh = newMesh;
+        }
+        #endregion
+
+        #region Drawing Hex Grid outlines
+        public Material lineMaterial;
+
+        List<Vector2> _gridPoints = new List<Vector2>();
+
+        void OnRenderObject() {
+            lineMaterial.SetPass(0);
+
+            GL.PushMatrix();
+            GL.MultMatrix(transform.localToWorldMatrix);
+            GL.Begin(GL.LINES);
+            {
+                for (int i = 0; i < _gridPoints.Count / 6; i++) {
+                    for (int j = 0; j < 6; j++) {
+                        GL.Vertex(_gridPoints[i * 6 + j]);
+                        GL.Vertex(_gridPoints[i * 6 + (j == 5 ? 0 : j + 1)]);
+                    }
+                }
+            }
+            GL.End();
+
+            GL.PopMatrix();
+        }
+        #endregion
+
+    }
+
+}
