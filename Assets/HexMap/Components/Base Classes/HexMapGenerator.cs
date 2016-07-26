886acc3f2568c04d6406889424834aeccc503eca
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2016-07-26 16:13:08 +0900

    Make Generator class abstract, add random seed.

---
+++ Assets/HexMap/Components/Base Classes/HexMapGenerator.cs
@@ -0,0 +1,33 @@
+﻿/* Copyright (c) 2016 Kevin Fischer
+ *
+ * This Source Code Form is subject to the terms of the MIT License.
+ * If a copy of the license was not distributed with this file,
+ * You can obtain one at https://opensource.org/licenses/MIT. */
+
+using UnityEngine;
+
+namespace HexMapEngine {
+
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(HexMapRenderer))]
+    public abstract class HexMapGenerator : MonoBehaviour {
+
+        public int seed;
+
+        System.Random _rng;
+        protected System.Random RNG { get { return _rng; } }
+
+        protected abstract HexMap GenerateMap();
+
+        void Start() {
+            OnValidate();
+        }
+
+        void OnValidate() {
+            _rng = new System.Random(seed);
+            GetComponent<HexMapRenderer>().HexMap = GenerateMap();
+        }
+
+    }
+
+}
---
+++ Assets/Sample Scene/Scripts/RadialMapGenerator.cs
@@ -0,0 +1,29 @@
+﻿/* Copyright (c) 2016 Kevin Fischer
+ *
+ * This Source Code Form is subject to the terms of the MIT License.
+ * If a copy of the license was not distributed with this file,
+ * You can obtain one at https://opensource.org/licenses/MIT. */
+
+using UnityEngine;
+using System.Collections.Generic;
+using HexMapEngine;
+
+public class RadialMapGenerator : HexMapGenerator {
+
+    public int radius = 0;
+
+    protected override HexMap GenerateMap() {
+        HexMap result = ScriptableObject.CreateInstance<HexMap>();
+        result.hideFlags = HideFlags.HideAndDontSave;
+        var hexCells = new List<HexCell>();
+        for (int q = -radius; q <= radius; q++) {
+            for (int r = Mathf.Max(-radius, -q - radius); r <= Mathf.Min(radius, -q + radius); r++) {
+                hexCells.Add(new HexCell(q, r, RNG.Next(4)));
+            }
+        }
+        result.SetHexCells(hexCells);
+
+        return result;
+    }
+
+}
