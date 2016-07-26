acc6a93ec723bf25c2042c815f7c62c43d7d7b30
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2016-07-26 12:34:59 +0900

    Add HexMap Generator and Sample scene.

---
+++ Assets/HexMap/Components/HexMapGenerator.cs
@@ -0,0 +1,35 @@
+﻿/* Copyright (c) 2016 Kevin Fischer
+ *
+ * This Source Code Form is subject to the terms of the MIT License.
+ * If a copy of the license was not distributed with this file,
+ * You can obtain one at https://opensource.org/licenses/MIT. */
+
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace HexMapEngine {
+
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(HexMapRenderer))]
+    public class HexMapGenerator : MonoBehaviour {
+
+        HexMapRenderer _renderer;
+
+        public static HexMap StandardMap() {
+            HexMap result = ScriptableObject.CreateInstance<HexMap>();
+            result.hideFlags = HideFlags.HideAndDontSave;
+            result.hexCells = new List<HexCell>();
+            result.hexCells.Add(new HexCell(0, 0));
+
+            return result;
+        }
+
+        void Start() {
+            _renderer = GetComponent<HexMapRenderer>();
+
+            _renderer.HexMap = StandardMap();
+        }
+
+    }
+
+}
