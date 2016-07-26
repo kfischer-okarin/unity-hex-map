849db47b68a3ae69e485f06949d738b96608c40e
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2016-07-26 12:28:35 +0900

    HexGrid and HexMap data classes Basic Hex Map representation using axial coordinates as specified in http://www.redblobgames.com/grids/hexagons/#coordinates

---
+++ Assets/HexMap/HexCell.cs
@@ -0,0 +1,21 @@
+﻿/* Copyright (c) 2016 Kevin Fischer
+ *
+ * This Source Code Form is subject to the terms of the MIT License.
+ * If a copy of the license was not distributed with this file,
+ * You can obtain one at https://opensource.org/licenses/MIT. */
+
+namespace HexMapEngine {
+
+    [System.Serializable]
+    public struct HexCell {
+
+        public int q, r;
+
+        public HexCell(int q, int r) {
+            this.q = q;
+            this.r = r;
+        }
+
+    }
+
+}
---
+++ Assets/HexMap/HexMap.cs
@@ -0,0 +1,18 @@
+﻿/* Copyright (c) 2016 Kevin Fischer
+ *
+ * This Source Code Form is subject to the terms of the MIT License.
+ * If a copy of the license was not distributed with this file,
+ * You can obtain one at https://opensource.org/licenses/MIT. */
+
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace HexMapEngine {
+
+    public class HexMap : ScriptableObject {
+
+        public List<HexCell> hexCells;
+
+    }
+
+}
